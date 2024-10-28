fddacae280aed20848ee53339f3f32f8eaa73a3d
VS hp <magnipython@gmail.com>
2024-10-29 00:32:40 +0100

    add coords to user

--- models/User.js
+++ models/User.js
@@ -88,6 +88,12 @@ const UserSchema = new mongoose.Schema(
       type: Array,
       default: [],
     },
+    latitude: {
+      type: Number,
+    },
+    longitude: {
+      type: Number,
+    },
     role: {
       type: Number,
       enum: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
