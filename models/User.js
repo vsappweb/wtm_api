3bc57f8eeedae7161daddec84b50b2a5d4a9f260
VS hp <magnipython@gmail.com>
2024-11-02 15:30:46 +0100

    fix add profile picture old

--- models/User.js
+++ models/User.js
@@ -26,7 +26,7 @@ const UserSchema = new mongoose.Schema(
       type: String,
       default: "",
     },
-    coverPicture: {
+    profilePictureOld: {
       type: String,
       default: "",
     },
