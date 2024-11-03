a1ab232b716e2cbe2252e7348008d14483115c4d
VS hp <magnipython@gmail.com>
2024-11-03 20:48:56 +0100

    fix api for images

--- index.js
+++ index.js
@@ -65,9 +65,41 @@ const storageAvatar = multer.diskStorage({
   },
 });
 
+const storageCountry = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images/slider/countries");
+  },
+  filename: (req, file, cb) => {
+    // cb(null, file.originalname);
+    cb(null, req.body.name);
+  },
+});
+
+const storageRegion = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images/slider/regions");
+  },
+  filename: (req, file, cb) => {
+    // cb(null, file.originalname);
+    cb(null, req.body.name);
+  },
+});
+
+const storageCity = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images/slider/cities");
+  },
+  filename: (req, file, cb) => {
+    // cb(null, file.originalname);
+    cb(null, req.body.name);
+  },
+});
+
 const upload = multer({ storage: storage });
 const uploadAvatar = multer({ storage: storageAvatar });
-
+const uploadCountry = multer({ storage: storageCountry });
+const uploadRegion = multer({ storage: storageRegion });
+const uploadCity = multer({ storage: storageCity });
 
 app.post("/api/upload", upload.any("file"), (req, res) => {
   try {
@@ -77,6 +109,27 @@ app.post("/api/upload", upload.any("file"), (req, res) => {
   }
 });
 app.post("/api/upload/userAvatar", uploadAvatar.any("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploaded successfully.");
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.post("/api/upload/country", uploadCountry.any("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploaded successfully.");
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.post("/api/upload/region", uploadRegion.any("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploaded successfully.");
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.post("/api/upload/city", uploadCity.any("file"), (req, res) => {
   try {
     return res.status(200).json("File uploaded successfully.");
   } catch (err) {
