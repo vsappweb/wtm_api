fca8f8e9b9ef39e0c34dbf1ec667c827d0c77ea2
VS hp <magnipython@gmail.com>
2024-11-10 21:25:09 +0100

    some changes

--- routes/deleteImg.js
+++ routes/deleteImg.js
@@ -4,42 +4,36 @@ const path = require("path");
 
 router.delete("/:filename", (req, res) => {
   const filename = req.params.filename;
-  const dirPath = req.body.dirPath;
+  const dirPath= req.body.dirPath;
 
-  if (!dirPath) {
-    const filePath = path.join(__dirname, "../public/images/", filename);
+  if (!filename || typeof filename !== "string") {
+    return res.status(400).json("Invalid filename");
+  }
 
-    if (!filename) {
-      return res.status(400).json("Filename is required");
-    }
+  if (dirPath !== undefined && typeof dirPath !== "string") {
+    return res.status(400).json("Invalid directory path");
+  }
+  if (dirPath === undefined) {
+    return res.status(400).json(`Invalid directory path undefined`);
+  }
 
-    try {
-      if (fs.existsSync(filePath)) {
-        fs.unlinkSync(filePath);
-        res.status(200).json("File deleted");
-      } else {
-        res.status(404).json("File not found");
-      }
-    } catch (err) {
-      res.status(500).json(err);
-    }
+  let filePath;
+  if (dirPath) {
+    filePath = path.join(__dirname, `../public/images/${dirPath}/`, filename);
   } else {
-    const filePath = path.join(
-      __dirname,
-      "../public/images/",
-      dirPath,
-      filename
-    );
-    try {
-      if (fs.existsSync(filePath)) {
-        fs.unlinkSync(filePath);
-        res.status(200).json("File deleted");
-      } else {
-        res.status(404).json("File not found");
-      }
-    } catch (err) {
-      res.status(500).json(err);
+    filePath = path.join(__dirname, "../public/images/", filename);
+  }
+
+  try {
+    if (fs.existsSync(filePath)) {
+      fs.unlinkSync(filePath);
+      res.status(200).json(`File deleted: ${dirPath} and ${filePath} .`);
+    } else {
+      res.status(404).json(`File not found: ${dirPath} and ${filePath} .`);
     }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(`An error occurred ${dirPath} and ${filePath} .`);
   }
 });
 
