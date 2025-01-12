1448713cb7517e056667a6dd1391ecee3dd6e640
VS hp <magnipython@gmail.com>
2025-01-12 21:25:09 +0100

    out ws server

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const ws = require("ws");
+// const ws = require("ws");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
@@ -23,57 +23,57 @@ const util = require("util");
 const path = require("path");
 const cors = require("cors");
 const port = process.env.PORT || 8800;
-const ws_port = process.env.WS_PORT || 8900;
-const cors_env = process.env.REACT_APP_ORIGIN;
+// const ws_port = process.env.WS_PORT || 8900;
+// const cors_env = process.env.REACT_APP_ORIGIN;
 
 dotenv.config();
 
-const wss = new ws.Server(
-  {
-    port: ws_port,
-    cors: { origin: "*" },
-  },
-  () => {
-    if (wss) {
-      console.log(`WebSocket Server is running  on port ${ws_port}`);
-    } else {
-      console.error("Error creating WebSocket Server");
-    }
-  }
-);
+// const wss = new ws.Server(
+//   {
+//     port: ws_port,
+//     cors: { origin: "*" },
+//   },
+//   () => {
+//     if (wss) {
+//       console.log(`WebSocket Server is running  on port ${ws_port}`);
+//     } else {
+//       console.error("Error creating WebSocket Server");
+//     }
+//   }
+// );
 
-wss.on("connection", (ws) => {
-  console.log("Client connected");
-  ws.on("message", (message) => {
-    console.log("Received message:", message);
-    message = JSON.parse(message);
-    switch (message.event) {
-      case "message":
-        broadcastMessage(message);
-        break;
-      case "connection":
-        broadcastMessage(message);
-        break;
-      case "disconnect":
-        broadcastMessage(message);
-        break;
-      default:
-        console.log("Invalid event type:", message.event);
-        break;
-    }
-  });
-  // ws.on("close", () => {
-  //   console.log("Client disconnected");
-  // });
-});
+// wss.on("connection", (ws) => {
+//   console.log("Client connected");
+//   ws.on("message", (message) => {
+//     console.log("Received message:", message);
+//     message = JSON.parse(message);
+//     switch (message.event) {
+//       case "message":
+//         broadcastMessage(message);
+//         break;
+//       case "connection":
+//         broadcastMessage(message);
+//         break;
+//       case "disconnect":
+//         broadcastMessage(message);
+//         break;
+//       default:
+//         console.log("Invalid event type:", message.event);
+//         break;
+//     }
+//   });
+//   ws.on("close", () => {
+//     console.log("Client disconnected");
+//   });
+// });
 
-function broadcastMessage(message) {
-  wss.clients.forEach((client) => {
-    // if (client.readyState === wss.OPEN) {
-      client.send(JSON.stringify(message));
-    // }
-  });
-}
+// function broadcastMessage(message) {
+//   wss.clients.forEach((client) => {
+//     // if (client.readyState === wss.OPEN) {
+//       client.send(JSON.stringify(message));
+//     // }
+//   });
+// }
 
 
 // mongoose.connect(process.env.MONGO_URL)
