d3c1f1456aa6b297f6fbe798534410368a8b9b7b
VS hp <magnipython@gmail.com>
2025-01-12 19:50:41 +0100

    test2

--- index.js
+++ index.js
@@ -28,6 +28,54 @@ const cors_env = process.env.REACT_APP_ORIGIN;
 
 dotenv.config();
 
+const wss = new ws.Server(
+  {
+    port: ws_port,
+    cors: { origin: "*" },
+  },
+  () => {
+    if (wss) {
+      console.log(`WebSocket Server is running  on port ${ws_port}`);
+    } else {
+      console.error("Error creating WebSocket Server");
+    }
+  }
+);
+
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+  ws.on("message", (message) => {
+    console.log("Received message:", message);
+    message = JSON.parse(message);
+    switch (message.event) {
+      case "message":
+        broadcastMessage(message);
+        break;
+      case "connection":
+        broadcastMessage(message);
+        break;
+      case "disconnect":
+        broadcastMessage(message);
+        break;
+      default:
+        console.log("Invalid event type:", message.event);
+        break;
+    }
+  });
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+function broadcastMessage(message) {
+  wss.clients.forEach((client) => {
+    if (client.readyState === ws.OPEN) {
+      client.send(JSON.stringify(message));
+    }
+  });
+}
+
+
 // mongoose.connect(process.env.MONGO_URL)
 try {
   mongoose.connect(process.env.MONGO_URL);
@@ -98,49 +146,3 @@ if (process.env.NODE_ENV_LOG === "production") {
   }
 }
 
-const wss = new ws.Server(
-  {
-    port: ws_port,
-    cors: { origin: "*" },
-  },
-  () => {
-    if (wss) {
-      console.log(`WebSocket Server is running  on port ${ws_port}`);
-    } else {
-      console.error("Error creating WebSocket Server");
-    }
-  }
-);
-
-wss.on("connection", (ws) => {
-  console.log("Client connected");
-  ws.on("message", (message) => {
-    console.log("Received message:", message);
-    message = JSON.parse(message);
-    switch (message.event) {
-      case "message":
-        broadcastMessage(message);
-        break;
-      case "connection":
-        broadcastMessage(message);
-        break;
-      case "disconnect":
-        broadcastMessage(message);
-        break;
-      default:
-        console.log("Invalid event type:", message.event);
-        break;
-    }
-  });
-  ws.on("close", () => {
-    console.log("Client disconnected");
-  });
-});
-
-function broadcastMessage(message) {
-  wss.clients.forEach((client) => {
-    if (client.readyState === ws.OPEN) {
-      client.send(JSON.stringify(message));
-    }
-  });
-}
